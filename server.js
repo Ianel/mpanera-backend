d05caa7e7f28e0b41af72c5100520cbbe19da3b6
Ianel <ianell.s118@gmail.com>
2022-05-13 06:45:47 +0300

    feat: add routes to server

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 5000;
 
+const usersRoutes = require('./routes/users.routes');
+const authRoutes = require('./routes/auth.routes');
+const housingRoutes = require('./routes/housing.routes');
+
+app.use('/api/v1/users', usersRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/housing', housingRoutes);
+
 app.listen(port, "localhost", (req, res) => {
     console.log(`Server running on port: ${port}`);
 });
