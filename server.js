afa322389f9b22a530fe93ca70d65de79083c331
Ianel <ianell.s118@gmail.com>
2022-05-14 16:54:56 +0300

    feat: add comments and cors modules

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
+// Third-party Modules
 const express = require('express');
-const dotenv = require("dotenv");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
-dotenv.config({ path: './.env' });
+// Reference environment variables
+dotenv.config({ path: '.env' });
 
+// Set Express and Port
 const app = express();
+const port = process.env.PORT || 5000;
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 5000;
-
+// Routers
 const usersRoutes = require('./routes/users.routes');
 const authRoutes = require('./routes/auth.routes');
 const housingRoutes = require('./routes/housing.routes');
 
+// Routes
 app.use('/api/v1/users', usersRoutes);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/housing', housingRoutes);
 
+// Server connection
 app.listen(port, "localhost", (req, res) => {
     console.log(`Server running on port: ${port}`);
 });
