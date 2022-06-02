f91b048ed1becf575352801af8b5616a96d13830
Ianel <ianell.s118@gmail.com>
2022-06-02 13:51:56 +0300

    tried to implement login controllers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,9 @@
-const controllers = require('../controllers/auth.controllers');
-const express = require('express');
+const controllers = require("../controllers/auth.controllers");
+const express = require("express");
 const router = express.Router();
 
-router 
-    .route('/login')
-    .post(controllers.login);
+router.route("/login").post(controllers.signin);
 
-router
-    .route('/register')
-    .post(controllers.register);
-
+router.route("/register").post(controllers.register);
 
 module.exports = router;
