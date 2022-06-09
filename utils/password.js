34895c57b9d07625eb0d7a41211e165f6800fd2d
Ianel <ianell.s118@gmail.com>
2022-06-09 16:38:39 +0300

    change comparePassword logic

--- utils/password.js
+++ utils/password.js
@@ -1,8 +1,10 @@
 const bcrypt = require("bcrypt");
 
 exports.hashPassword = async (password) => {
-    const salt = await bcrypt.genSalt();
+    const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     return hashedPassword;
 };
+
+exports.comparePassword = async (password, hashedPassword) => await bcrypt.compare(password, hashedPassword);;
