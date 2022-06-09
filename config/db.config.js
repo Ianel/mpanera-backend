253f5de473498c430c1285337be34bdf14ce8ac5
Ianel <ianell.s118@gmail.com>
2022-06-09 14:08:24 +0300

    configuration for db

--- config/db.config.js
+++ config/db.config.js
@@ -1,80 +1,14 @@
-const { Pool, Client } = require("pg");
+const { Pool } = require("pg");
 
-const connectWithPool = () => {
-  const pool = new Pool({
+const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
-  });
-
-  pool.connect();
-
-  return pool;
-};
-
-/* const connectWithClient = () => {
-  const client = new Client({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-  });
+    port: process.env.DB_PORT
+});
 
-  client.connect();
-
-  client.on("connect", () => {
-    console.log("Database connected");
-  });
-
-  return client;
+module.exports = {
+    query: async (text, params) => await pool.query(text, params),
+    end: () => pool.end()
 };
-
-const client = connectWithClient();
- */
-const pool = connectWithPool();
-
-module.exports = [pool];
-
-// Database connection
-
-/* async function poolDemo() {
-    const pool = new Pool({
-        host: "localhost",
-        user: "postgres",
-        password: "postgres",
-        port: 5432,
-        database: "mpanera",
-    });
-    const now = await pool.query("SELECT * FROM users;");
-    await pool.end();
-
-    return now;
-} */
-
-/* async function clientDemo() {
-    const client = new Client({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password:process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME,
-    });
-    client.connect();
-    const now = await client.query("SELECT * FROM users;");
-    await client.end();
-
-    return now;
-} */
-
-/* (async () => {
-   /*  const poolResult = await poolDemo();
-    console.log(poolResult["rows"]); */
-/* 
-    const clientResult = await clientDemo();
-    console.log({ clientResult });  
-  })(); */
-
-//module.exports = pool;
---
+++ config/db.sql
@@ -0,0 +1,124 @@
+CREATE TABLE users (
+    user_id SERIAL NOT NULL,
+    city CHARACTER VARYING(50),
+    postal_code INTEGER,
+    region CHARACTER VARYING(50),
+    country CHARACTER VARYING(50),
+    firstname CHARACTER VARYING(25),
+    lastname CHARACTER VARYING(50),
+    phone_number CHARACTER VARYING(100) UNIQUE NOT NULL,
+    email CHARACTER VARYING(100) UNIQUE,
+    password CHARACTER VARYING(200) NOT NULL,
+    gender CHARACTER VARYING(10),
+    birthdate DATE DEFAULT CURRENT_DATE,
+    adress CHARACTER VARYING(200),
+    facebook_name CHARACTER VARYING(200), 
+    profile_avatar CHARACTER VARYING(50), 
+    PRIMARY KEY (user_id) 
+);
+
+CREATE TABLE houses (
+    house_id SERIAL NOT NULL,
+    area CHARACTER VARYING(20) NOT NULL,
+    label CHARACTER VARYING(100) NOT NULL,
+    city CHARACTER VARYING(50) NOT NULL,
+    postal_code INTEGER NOT NULL,
+    region CHARACTER VARYING(50) NOT NULL,
+    country CHARACTER VARYING(50) NOT NULL,
+    adress CHARACTER VARYING(200) NOT NULL,
+    rent_price CHARACTER VARYING(20) NOT NULL,
+    rooms_number INTEGER NOT NULL,
+    description TEXT NOT NULL,
+    state CHARACTER VARYING(20) NOT NULL,
+    end_date DATE NOT NULL,
+    open_date DATE,
+    published_on DATE DEFAULT CURRENT_DATE,
+    PRIMARY KEY (house_id)
+);
+
+CREATE TABLE house_type (
+    house_type_id SERIAL NOT NULL,
+    type CHARACTER VARYING(20) NOT NULL,
+    PRIMARY KEY (house_type_id)
+);
+
+CREATE TABLE house_photo (
+    house_photo_id SERIAL NOT NULL,
+    path CHARACTER VARYING(50) NOT NULL,
+    PRIMARY KEY (house_photo_id)
+);
+
+CREATE TABLE rooms (
+    room_id SERIAL NOT NULL,
+    type CHARACTER VARYING(20) NOT NULL,
+    quantity INTEGER NOT NULL,
+    PRIMARY KEY (rooom_id)
+);
+
+CREATE TABLE services (
+    services_id SERIAL NOT NULL,
+    interior_toilets boolean NOT NULL,
+    outdoor_toilets boolean NOT NULL,
+    running_water boolean NOT NULL,
+    garage boolean NOT NULL,
+    pool boolean NOT NULL,
+    garden boolean NOT NULL,
+    accessibility CHARACTER VARYING (100),
+    PRIMARY KEY (services_id)
+);
+
+CREATE TABLE favorite (
+    favorite_id SERIAL NOT NULL
+);
+
+CREATE TABLE offers (
+    offers_id SERIAL NOT NULL
+);
+
+ALTER TABLE houses
+    ADD FOREIGN KEY (house_type_id)
+    REFERENCES house_type (house_type_id)
+    ON DELETE SET NULL
+    NOT VALID;
+
+ALTER TABLE houses
+    ADD FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+    ON DELETE CASCADE
+    NOT VALID;
+
+ALTER TABLE favorite
+    ADD FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+    ON DELETE SET NULL
+    NOT VALID;
+
+ALTER TABLE favorite
+    ADD FOREIGN KEY (house_id)
+    REFERENCES houses (house_id)
+    ON DELETE SET NULL
+    NOT VALID;
+
+ALTER TABLE house_photo
+    ADD FOREIGN KEY (house_id)
+    REFERENCES houses (house_id)
+    ON DELETE SET NULL
+    NOT VALID;
+
+ALTER TABLE rooms
+    ADD FOREIGN KEY (house_id)
+    REFERENCES houses (house_id)
+    ON DELETE SET NULL
+    NOT VALID;
+
+ALTER TABLE offers
+    ADD FOREIGN KEY (house_id)
+    REFERENCES houses (house_id)
+    ON DELETE SET NULL
+    NOT VALID;
+
+ALTER TABLE offers
+    ADD FOREIGN KEY (services_id)
+    REFERENCES services (services_id)
+    ON DELETE SET NULL
+    NOT VALID;
