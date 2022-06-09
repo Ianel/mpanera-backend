20205f0b4c2ebaa41cffd0dfbe935cd330557585
Ianel <ianell.s118@gmail.com>
2022-06-09 16:37:37 +0300

    add login request

--- db/auth.db.js
+++ db/auth.db.js
@@ -1,20 +1,21 @@
 const pool = require("../config/db.config");
 
 const getUserByTel = async (tel) => {
-    const user = await pool.query("SELECT phone_number FROM users WHERE phone_number = $1",
+    const { rows: user } = await pool.query("SELECT * FROM users WHERE phone_number = $1",
     [tel]);
     
     return user[0];
 };
 
-const createUserDb = async (phoneNumber, password) => {
+const createUserDb = async (tel, password) => {
     const { rows: user } = await pool.query("INSERT INTO users(phone_number, password) VALUES($1, $2) RETURNING phone_number", 
-    [phoneNumber, password]);
+    [tel, password]);
 
     return user[0];
 };
 
+
 module.exports = {
     getUserByTel,
-    createUserDb
+    createUserDb,
 };
