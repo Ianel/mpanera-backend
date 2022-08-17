1912c7b1285805169fa87f93d6adf51b3117b22a
Ianel <ianell.s118@gmail.com>
2022-08-17 17:01:51 +0300

    logic for updateUser, image upload not implemented

--- db/users.db.js
+++ db/users.db.js
@@ -17,10 +17,44 @@ const getUserDb = async (id) => {
   return user[0];
 };
 
-const updateUserDb = async (id, { firstname, lastname }) => {
+const updateUserDb = async (
+  id,
+  {
+    firstname,
+    lastname,
+    email,
+    phone_number,
+    city,
+    country,
+    postal_code,
+    region,
+    gender,
+    birthdate,
+    adress,
+    facebook_name,
+    profile_avatar,
+    about,
+  }
+) => {
   const { rows: user } = await pool.query(
-    "UPDATE users SET firstname = $1, lastname = $2 WHERE user_id = $3;",
-    [firstname, lastname, id]
+    "UPDATE users SET firstname = $1, lastname = $2, email = $3, phone_number = $4, city = $5, country = $6, postal_code = $7, region = $8, gender = $9, birthdate = $10, adress = $11, facebook_name = $12, profile_avatar = $13, about = $14 WHERE user_id = $15;",
+    [
+      firstname,
+      lastname,
+      email,
+      phone_number,
+      city,
+      country,
+      postal_code,
+      region,
+      gender,
+      birthdate,
+      adress,
+      facebook_name,
+      profile_avatar,
+      about,
+      id,
+    ]
   );
 
   return user[0];
