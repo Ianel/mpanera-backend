f8781390858d977c12840c3646962061aa5dd9d7
Ianel <ianell.s118@gmail.com>
2022-08-22 10:37:56 +0300

    returns user_id on creating a user

--- db/auth.db.js
+++ db/auth.db.js
@@ -1,21 +1,24 @@
 const pool = require("../config/db.config");
 
 const getUserByTel = async (tel) => {
-    const { rows: user } = await pool.query("SELECT * FROM users WHERE phone_number = $1",
-    [tel]);
-    
-    return user[0];
+  const { rows: user } = await pool.query(
+    "SELECT * FROM users WHERE phone_number = $1",
+    [tel]
+  );
+
+  return user[0];
 };
 
 const createUserDb = async (tel, password) => {
-    const { rows: user } = await pool.query("INSERT INTO users(phone_number, password) VALUES($1, $2) RETURNING phone_number", 
-    [tel, password]);
+  const { rows: user } = await pool.query(
+    "INSERT INTO users(phone_number, password) VALUES($1, $2) RETURNING phone_number, user_id",
+    [tel, password]
+  );
 
-    return user[0];
+  return user[0];
 };
 
-
 module.exports = {
-    getUserByTel,
-    createUserDb,
+  getUserByTel,
+  createUserDb,
 };
