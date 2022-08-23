a891c2f8663a9323b5402c0d258e4d0541d81338
Ianel <ianell.s118@gmail.com>
2022-08-23 09:10:52 +0300

    new images uploaded

--- db/houses.db.js
+++ db/houses.db.js
@@ -110,7 +110,7 @@ const updateHouseDb = async (
     state,
     end_date,
     open_date,
-    house_type_id,
+    house_type,
     user_id,
   }
 ) => {
@@ -119,12 +119,12 @@ const updateHouseDb = async (
     postal_code = $4, region = $5, country = $6, adress = $7,
     rent_price = $8, rooms_number = $9, description = $10,
     state = $11, end_date = $12, open_date = $13, published_on = $14,
-    house_type_id = $15, user_id = $16 WHERE house_id = $17 
+    house_type = $15, user_id = $16 WHERE house_id = $17 
     RETURNING 
         house_id, area, label, city, postal_code, region,
         country, adress,rent_price, rooms_number, 
         description, state, end_date, open_date, 
-        published_on, house_type_id, user_id
+        published_on, house_type, user_id
     `,
     [
       area,
@@ -141,7 +141,7 @@ const updateHouseDb = async (
       end_date,
       open_date,
       publishedOn,
-      house_type_id,
+      house_type,
       user_id,
       id,
     ]
